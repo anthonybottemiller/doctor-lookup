ab8fd839333a1fa50ba55f5bf2a70278b3ff5395
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-01-06 14:37:30 -0800

    add code to use getDoctors method properly

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -1,8 +1,9 @@
-var Doctor = require('./../js/doctor.js').doctorModule;
+var doctor = require('./../js/doctor.js').doctorModule;
 
 $(document).ready(function() {
-  var doctorObject = new Doctor();
-  $("#get-doctors").click(function() {
-      doctorObject.getDoctors();
+  var doctorObject = new doctor();
+  $("#get-doctors").click(function(){
+    var medicalIssue = $('#issue').val();
+    doctorObject.getDoctors(medicalIssue);
   });
 });
