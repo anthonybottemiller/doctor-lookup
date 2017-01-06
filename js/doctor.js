ea017675bbdc1b59d3fa2efb6d59e94c2c6e7643
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-01-06 14:38:41 -0800

    add getters and setters as well as writing doc names to page

--- js/doctor.js
+++ js/doctor.js
@@ -1,17 +1,42 @@
 var apiKey = require('./../.env').apiKey;
 
-function Doctor(){
+function doctor(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
 }
 
-Doctor.prototype.getDoctors = function(medicalIssue) {
+doctor.prototype.getFirstName = function() {
+    return this.firstName;
+};
+
+doctor.prototype.getLastName = function() {
+    return this.lastName;
+};
+
+doctor.prototype.getDoctors = function(medicalIssue) {
     console.log("Works so far!");
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
+    $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + medicalIssue + "&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=" + apiKey)
+    .then(function(result) {
+      var doctors = [];
+      if (result.data.length === 0){
+          console.log("result empty");
+      }
+
+      else {
+        result.data.forEach(function(data) {
+          var newDoctor = new doctor(data.profile.first_name, data.profile.last_name);
+          doctors.push(newDoctor);
+          return doctors;
+        });
+      }
+        doctors.forEach(function(doctor) {
+          $("ul#found-doctors").append("<li>" + doctor.getFirstName() + " " + doctor.getLastName() + "</li>");
+        });
+
     })
    .fail(function(error){
       console.log("fail");
     });
 };
 
-exports.doctorModule = Doctor;
+exports.doctorModule = doctor;
