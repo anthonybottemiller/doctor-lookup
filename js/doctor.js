55253050bf6212dc0b45eb119210daecd51bb1e2
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-01-08 13:51:10 -0800

    Fix code format and remove a bad looking return

--- js/doctor.js
+++ js/doctor.js
@@ -1,46 +1,45 @@
 var apiKey = require('./../.env').apiKey;
 
 function doctor(firstName, lastName){
-    this.firstName = firstName;
-    this.lastName = lastName;
+  this.firstName = firstName;
+  this.lastName = lastName;
 }
 
 doctor.prototype.getFirstName = function() {
-    return this.firstName;
+  return this.firstName;
 };
 
 doctor.prototype.getLastName = function() {
-    return this.lastName;
+  return this.lastName;
 };
 
 doctor.prototype.getDoctors = function(medicalIssue) {
 
-    $("ul#found-doctors").empty();
+  $("ul#found-doctors").empty();
 
-    $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + medicalIssue + "&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=" + apiKey)
-    .then(function(result) {
-      var doctors = [];
-      if (result.data.length === 0){
-          console.log("result empty");
-          $("ul#found-doctors").append("<li>Your search did not return any results!</li>");
-      }
+  $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + medicalIssue + "&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=" + apiKey)
 
-      else {
-        result.data.forEach(function(data) {
-          var newDoctor = new doctor(data.profile.first_name, data.profile.last_name);
-          doctors.push(newDoctor);
-          return doctors;
-        });
-      }
+  .then(function(result) {
+    var doctors = [];
+    if (result.data.length === 0){
+      $("ul#found-doctors").append("<li>Your search did not return any results!</li>");
+    }
 
-        doctors.forEach(function(doctor) {
-          $("ul#found-doctors").append("<li>" + doctor.getFirstName() + " " + doctor.getLastName() + "</li>");
-        });
+    else {
+      result.data.forEach(function(data) {
+        var newDoctor = new doctor(data.profile.first_name, data.profile.last_name);
+        doctors.push(newDoctor);
+      });
+    }
 
-    })
-   .fail(function(error){
-      console.log("fail");
+    doctors.forEach(function(doctor) {
+      $("ul#found-doctors").append("<li>" + doctor.getFirstName() + " " + doctor.getLastName() + "</li>");
     });
+  })
+
+  .fail(function(error){
+    console.log("fail");
+  });
 };
 
 exports.doctorModule = doctor;
