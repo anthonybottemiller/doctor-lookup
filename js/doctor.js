4be6ca891899682888435c5db1165b88a894407f
Anthony Bottemiller <anthonybottemiller@outlook.com>
2017-01-08 13:34:08 -0800

    add missing semicolon

--- js/doctor.js
+++ js/doctor.js
@@ -15,7 +15,7 @@ doctor.prototype.getLastName = function() {
 
 doctor.prototype.getDoctors = function(medicalIssue) {
 
-    $("ul#found-doctors").empty()
+    $("ul#found-doctors").empty();
 
     $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + medicalIssue + "&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=" + apiKey)
     .then(function(result) {
@@ -32,7 +32,7 @@ doctor.prototype.getDoctors = function(medicalIssue) {
           return doctors;
         });
       }
-      
+
         doctors.forEach(function(doctor) {
           $("ul#found-doctors").append("<li>" + doctor.getFirstName() + " " + doctor.getLastName() + "</li>");
         });
